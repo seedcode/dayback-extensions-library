6f845e86659b36ac29739b0642943aee0d38f8d8
Michal Dabrowski <mdabrowski@gmail.com>
2023-02-23 14:25:32 -0800

    Adding Track UPS Button to library

---
+++ track-ups/trackUPS.js
@@ -0,0 +1,157 @@
+// Track UPS Packages v1.1
+
+// Purpose:
+// This button action will look through an event's
+// description and will automatically open the first
+// UPS package code that it finds.
+//
+// Action Type: Button Action
+// Open In New Window: Yes
+
+// More info on custom App Actions here:
+// https://docs.dayback.com/article/140-custom-app-actions
+
+// Declare globals
+var options = {};
+var inputs = {};
+
+try {
+	//----------- Configuration -------------------
+
+	// Options specified for this action
+
+	// Seconds to wait to allow this action to run before reporting an error (set to 0 to deactivate)
+
+	options.runTimeout = 0;
+
+	// Array of account emails for whom this action will run. Leave blank to allow the action to run for everyone.
+	// Example: ['person@domain.com', 'someone@domain.com']
+
+	options.restrictedToAccounts = [];
+
+	// Define the Variable you want to scan for the UPS code
+
+	inputs.scanVariable = event.description;
+
+	//----------- End Configuration -------------------
+} catch (error) {
+	reportError(error);
+}
+
+// ----------- The action itself: -------------------
+
+// Action code goes inside this function
+function run() {
+	//Define where we are reading ups numbers from
+
+	console.log(event);
+	var searchText = inputs.scanVariable;
+
+	//Specify the ups tracking base url
+	var upsURL =
+		'http://wwwapps.ups.com/WebTracking/track?loc=en_US&track.x=Track&trackNums=';
+
+	// urls will be an array of URL matches
+	var upsTracking = searchText.match(
+		/\b(1Z ?[0-9A-Z]{3} ?[0-9A-Z]{3} ?[0-9A-Z]{2} ?[0-9A-Z]{4} ?[0-9A-Z]{3} ?[0-9A-Z]|[\dT]\d\d\d ?\d\d\d\d ?\d\d\d)\b/
+	);
+
+	// you could then iterate through urls but we just want the first one
+	if (upsTracking) {
+		open(upsURL + upsTracking[0]);
+	}
+}
+
+//----------- Run function wrapper and helpers - you shouldn’t need to edit below this line. -------------------
+
+// Variables used for helper functions below
+var timeout;
+
+// Execute the run function as defined above
+try {
+	if (
+		!options.restrictedToAccounts ||
+		!options.restrictedToAccounts.length ||
+		(options.restrictedToAccounts &&
+			options.restrictedToAccounts.indexOf(inputs.account) > -1)
+	) {
+		if (action.preventDefault && options.runTimeout) {
+			timeoutCheck();
+		}
+		run();
+	} else if (action.preventDefault) {
+		confirmCallback();
+	}
+} catch (error) {
+	reportError(error);
+}
+
+// Run confirm callback when preventDefault is true. Used for async actions
+function confirmCallback() {
+	cancelTimeoutCheck();
+	if (action.callbacks.confirm) {
+		action.callbacks.confirm();
+	}
+}
+
+// Run cancel callback when preventDefault is true. Used for async actions
+function cancelCallback() {
+	cancelTimeoutCheck();
+	if (action.callbacks.cancel) {
+		action.callbacks.cancel();
+	}
+}
+
+// Check if the action has run within the specified time limit when preventDefault is enabled
+function timeoutCheck() {
+	timeout = setTimeout(
+		function () {
+			var error = {
+				name: 'Timeout',
+				message:
+					'The action was unable to execute within the allotted time and has been stopped',
+			};
+			reportError(error, true);
+		},
+		options && options.runTimeout ? options.runTimeout * 1000 : 0
+	);
+}
+
+function cancelTimeoutCheck() {
+	if (timeout) {
+		clearTimeout(timeout);
+	}
+}
+
+// Function to report any errors that occur when running this action
+// Follows standard javascript error reporter format of an object with name and message properties
+function reportError(error) {
+	var errorTitle = 'Error Running Custom Action';
+	var errorMessage =
+		'<p>There was a problem running the action "<span style="white-space: nowrap">' +
+		action.name +
+		'</span>"</p><p>Error: ' +
+		error.message +
+		'.</p><p>This may result in unexpected behavior of the calendar.</p>';
+	if (action.preventDefault && timeout) {
+		confirmCallback();
+	} else {
+		cancelCallback();
+	}
+
+	setTimeout(function () {
+		utilities.showModal(
+			errorTitle,
+			errorMessage,
+			null,
+			null,
+			'OK',
+			null,
+			null,
+			null,
+			true,
+			null,
+			true
+		);
+	}, 1000);
+}
