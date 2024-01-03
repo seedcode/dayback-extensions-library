8f49c8076e2fff1c54d97043bc3eddf4705b997f
Michal Dabrowski <mdabrowski@gmail.com>
2024-01-02 16:07:00 -0800

    Bug fix for lost eventID context

--- open-record-on-double-click/open_record_on_double_click_part1.js
+++ open-record-on-double-click/open_record_on_double_click_part1.js
@@ -17,73 +17,258 @@ var options = {};
 var inputs = {};
 
 try {
-  //----------- Configuration -------------------
+	//----------- Configuration -------------------
 
-  // Seconds to wait to allow this action to run before reporting an error (set to 0 to deactivate)
-  // Leave this set to 0 to avoid unexpected behavior
+	// Seconds to wait to allow this action to run before reporting an error (set to 0 to deactivate)
+	// Leave this set to 0 to avoid unexpected behavior
 
-  options.runTimeout = 0;
+	options.runTimeout = 0;
 
-  // Double Click Action:
-  // --------------------
+	// Double Click Action:
+	// --------------------
 
-  // The code below will run when an event is double-clicked.
-  // If you want Double Click to run on certain calendars, you
-  // you can first check the contents of the variable 'calendarName'
-  // to determine if a record should open.
+	// The code below will run when an event is double-clicked.
+	// If you want Double Click to run on certain calendars, you
+	// you can first check the contents of the variable 'calendarName'
+	// to determine if a record should open.// Open Record On Double Click v1.0 - Part 1
+	//
+	// Purpose:
+	// Double clicking an event will open up the native
+	// Salesforce or Filemaker record. Single clicking
+	// will open the native DayBack popover
+	//
+	// Action Type: After Events Rendered
+	// Prevent Default Action: No
+	//
+	// More info on custom actions here:
+	// https://docs.dayback.com/article/140-custom-app-actions
 
-  options.runOnDoubleClick = (calendarName) => {
-    // Opens up the native Salesforce record for the event
-    fbk.publish("dbk.navigate", { url: "/[[Id]]", new: false });
-  };
+	// Declare globals
 
-  //----------- End Configuration -------------------
+	var options = {};
+	var inputs = {};
+
+	try {
+		//----------- Configuration -------------------
+
+		// Seconds to wait to allow this action to run before reporting an error (set to 0 to deactivate)
+		// Leave this set to 0 to avoid unexpected behavior
+
+		options.runTimeout = 0;
+
+		// Double Click Action:
+		// --------------------
+
+		// The code below will run when an event is double-clicked.
+		// If you want Double Click to run on certain calendars, you
+		// you can first check the contents of the variable 'calendarName'
+		// to determine if a record should open.
+
+		options.runOnDoubleClick = (calendarName, eventID) => {
+			// Opens up the native Salesforce record for the event
+			fbk.publish('dbk.navigate', {url: '/' + eventID, new: true});
+		};
+
+		//----------- End Configuration -------------------
+	} catch (error) {
+		reportError(error);
+	}
+
+	//----------- The action itself: you may not need to edit this. -------------------
+
+	// Action code goes inside this function
+	function run() {
+		// Get all events
+
+		var clientEvents = seedcodeCalendar
+			.get('element')
+			.fullCalendar('clientEvents');
+
+		// Loop through events
+
+		clientEvents.forEach((event) => {
+			// Get all on-screen elements that represent the event
+			let divs = document.querySelectorAll(
+				'[data-id="' + event._id + '"]'
+			);
+
+			// Loop through each event and add a double click handler
+			// that runs your own double click function, then closes
+			// the popover in the background
+
+			divs.forEach((d) => {
+				if (d.dataset.hasOwnProperty('dblclickhandler')) {
+					return;
+				}
+
+				d.dataset.dblclickhandler = 1;
+
+				d.addEventListener('dblclick', (e) => {
+					seedcodeCalendar.init('dblclickrunning', true);
+
+					setTimeout(() => {
+						seedcodeCalendar.init('dblclickrunning', false);
+					}, 500);
+
+					options.runOnDoubleClick(
+						event.schedule.name,
+						event.eventID
+					);
+
+					$timeout(function () {
+						$rootScope.$broadcast('closePopovers');
+					}, 250);
+				});
+			});
+		});
+	}
+
+	//----------- Run function wrapper and helpers - you shouldn’t need to edit below this line. -------------------
+
+	// Variables used for helper functions below
+	var timeout;
+
+	// Execute the run function as defined above
+	try {
+		if (
+			!options.restrictedToAccounts ||
+			!options.restrictedToAccounts.length ||
+			(options.restrictedToAccounts &&
+				options.restrictedToAccounts.indexOf(inputs.account) > -1)
+		) {
+			if (action.preventDefault && options.runTimeout) {
+				timeoutCheck();
+			}
+			run();
+		} else if (action.preventDefault) {
+			confirmCallback();
+		}
+	} catch (error) {
+		reportError(error);
+	}
+
+	// Run confirm callback when preventDefault is true. Used for async actions
+	function confirmCallback() {
+		cancelTimeoutCheck();
+		if (action.callbacks.confirm) {
+			action.callbacks.confirm();
+		}
+	}
+
+	// Run cancel callback when preventDefault is true. Used for async actions
+	function cancelCallback() {
+		cancelTimeoutCheck();
+		if (action.callbacks.cancel) {
+			action.callbacks.cancel();
+		}
+	}
+
+	// Check if the action has run within the specified time limit when preventDefault is enabled
+	function timeoutCheck() {
+		timeout = setTimeout(
+			function () {
+				var error = {
+					name: 'Timeout',
+					message:
+						'The action was unable to execute within the allotted time and has been stopped',
+				};
+				reportError(error, true);
+			},
+			options && options.runTimeout ? options.runTimeout * 1000 : 0
+		);
+	}
+
+	function cancelTimeoutCheck() {
+		if (timeout) {
+			clearTimeout(timeout);
+		}
+	}
+
+	// Function to report any errors that occur when running this action
+	// Follows standard javascript error reporter format of an object with name and message properties
+	function reportError(error) {
+		var errorTitle = 'Error Running Custom Action';
+		var errorMessage =
+			'<p>There was a problem running the action "<span style="white-space: nowrap">' +
+			action.name +
+			'</span>"</p><p>Error: ' +
+			error.message +
+			'.</p><p>This may result in unexpected behavior of the calendar.</p>';
+		if (action.preventDefault && action.category !== event && timeout) {
+			confirmCallback();
+		} else {
+			cancelCallback();
+		}
+
+		setTimeout(function () {
+			utilities.showModal(
+				errorTitle,
+				errorMessage,
+				null,
+				null,
+				'OK',
+				null,
+				null,
+				null,
+				true,
+				null,
+				true
+			);
+		}, 1000);
+	}
+
+	options.runOnDoubleClick = (calendarName, recordId) => {
+		// Opens up the native Salesforce record for the event
+		fbk.publish('dbk.navigate', {url: '/' + recordId, new: true});
+	};
+
+	//----------- End Configuration -------------------
 } catch (error) {
-  reportError(error);
+	reportError(error);
 }
 
 //----------- The action itself: you may not need to edit this. -------------------
 
 // Action code goes inside this function
 function run() {
-  // Get all events
+	// Get all events
 
-  var clientEvents = seedcodeCalendar
-    .get("element")
-    .fullCalendar("clientEvents");
+	var clientEvents = seedcodeCalendar
+		.get('element')
+		.fullCalendar('clientEvents');
 
-  // Loop through events
+	// Loop through events
 
-  clientEvents.forEach((event) => {
-    // Get all on-screen elements that represent the event
-    let divs = document.querySelectorAll('[data-id="' + event._id + '"]');
+	clientEvents.forEach((event) => {
+		// Get all on-screen elements that represent the event
+		let divs = document.querySelectorAll('[data-id="' + event._id + '"]');
 
-    // Loop through each event and add a double click handler
-    // that runs your own double click function, then closes
-    // the popover in the background
+		// Loop through each event and add a double click handler
+		// that runs your own double click function, then closes
+		// the popover in the background
 
-    divs.forEach((d) => {
-      if (d.dataset.hasOwnProperty("dblclickhandler")) {
-        return;
-      }
+		divs.forEach((d) => {
+			if (d.dataset.hasOwnProperty('dblclickhandler')) {
+				return;
+			}
 
-      d.dataset.dblclickhandler = 1;
+			d.dataset.dblclickhandler = 1;
 
-      d.addEventListener("dblclick", (e) => {
-        seedcodeCalendar.init("dblclickrunning", true);
+			d.addEventListener('dblclick', (e) => {
+				seedcodeCalendar.init('dblclickrunning', true);
 
-        setTimeout(() => {
-          seedcodeCalendar.init("dblclickrunning", false);
-        }, 500);
+				setTimeout(() => {
+					seedcodeCalendar.init('dblclickrunning', false);
+				}, 500);
 
-        options.runOnDoubleClick(event.schedule.name);
+				options.runOnDoubleClick(event.schedule.name, event.eventID);
 
-        $timeout(function () {
-          $rootScope.$broadcast("closePopovers");
-        }, 250);
-      });
-    });
-  });
+				$timeout(function () {
+					$rootScope.$broadcast('closePopovers');
+				}, 250);
+			});
+		});
+	});
 }
 
 //----------- Run function wrapper and helpers - you shouldn’t need to edit below this line. -------------------
@@ -93,89 +278,89 @@ var timeout;
 
 // Execute the run function as defined above
 try {
-  if (
-    !options.restrictedToAccounts ||
-    !options.restrictedToAccounts.length ||
-    (options.restrictedToAccounts &&
-      options.restrictedToAccounts.indexOf(inputs.account) > -1)
-  ) {
-    if (action.preventDefault && options.runTimeout) {
-      timeoutCheck();
-    }
-    run();
-  } else if (action.preventDefault) {
-    confirmCallback();
-  }
+	if (
+		!options.restrictedToAccounts ||
+		!options.restrictedToAccounts.length ||
+		(options.restrictedToAccounts &&
+			options.restrictedToAccounts.indexOf(inputs.account) > -1)
+	) {
+		if (action.preventDefault && options.runTimeout) {
+			timeoutCheck();
+		}
+		run();
+	} else if (action.preventDefault) {
+		confirmCallback();
+	}
 } catch (error) {
-  reportError(error);
+	reportError(error);
 }
 
 // Run confirm callback when preventDefault is true. Used for async actions
 function confirmCallback() {
-  cancelTimeoutCheck();
-  if (action.callbacks.confirm) {
-    action.callbacks.confirm();
-  }
+	cancelTimeoutCheck();
+	if (action.callbacks.confirm) {
+		action.callbacks.confirm();
+	}
 }
 
 // Run cancel callback when preventDefault is true. Used for async actions
 function cancelCallback() {
-  cancelTimeoutCheck();
-  if (action.callbacks.cancel) {
-    action.callbacks.cancel();
-  }
+	cancelTimeoutCheck();
+	if (action.callbacks.cancel) {
+		action.callbacks.cancel();
+	}
 }
 
 // Check if the action has run within the specified time limit when preventDefault is enabled
 function timeoutCheck() {
-  timeout = setTimeout(
-    function () {
-      var error = {
-        name: "Timeout",
-        message:
-          "The action was unable to execute within the allotted time and has been stopped",
-      };
-      reportError(error, true);
-    },
-    options && options.runTimeout ? options.runTimeout * 1000 : 0
-  );
+	timeout = setTimeout(
+		function () {
+			var error = {
+				name: 'Timeout',
+				message:
+					'The action was unable to execute within the allotted time and has been stopped',
+			};
+			reportError(error, true);
+		},
+		options && options.runTimeout ? options.runTimeout * 1000 : 0
+	);
 }
 
 function cancelTimeoutCheck() {
-  if (timeout) {
-    clearTimeout(timeout);
-  }
+	if (timeout) {
+		clearTimeout(timeout);
+	}
 }
 
 // Function to report any errors that occur when running this action
 // Follows standard javascript error reporter format of an object with name and message properties
 function reportError(error) {
-  var errorTitle = "Error Running Custom Action";
-  var errorMessage =
-    '<p>There was a problem running the action "<span style="white-space: nowrap">' +
-    action.name +
-    '</span>"</p><p>Error: ' +
-    error.message +
-    ".</p><p>This may result in unexpected behavior of the calendar.</p>";
-  if (action.preventDefault && action.category !== event && timeout) {
-    confirmCallback();
-  } else {
-    cancelCallback();
-  }
+	var errorTitle = 'Error Running Custom Action';
+	var errorMessage =
+		'<p>There was a problem running the action "<span style="white-space: nowrap">' +
+		action.name +
+		'</span>"</p><p>Error: ' +
+		error.message +
+		'.</p><p>This may result in unexpected behavior of the calendar.</p>';
+	if (action.preventDefault && action.category !== event && timeout) {
+		confirmCallback();
+	} else {
+		cancelCallback();
+	}
 
-  setTimeout(function () {
-    utilities.showModal(
-      errorTitle,
-      errorMessage,
-      null,
-      null,
-      "OK",
-      null,
-      null,
-      null,
-      true,
-      null,
-      true
-    );
-  }, 1000);
+	setTimeout(function () {
+		utilities.showModal(
+			errorTitle,
+			errorMessage,
+			null,
+			null,
+			'OK',
+			null,
+			null,
+			null,
+			true,
+			null,
+			true
+		);
+	}, 1000);
 }
