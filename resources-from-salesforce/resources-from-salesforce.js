b25c5e9fd0d6b4ae58283748f4e43d285324f51e
KC Embrey <kc@thechilibox.com>
2025-05-06 10:08:10 -0600

    Resources from Salesforce: Added location and color by resource features

--- resources-from-salesforce/resources-from-salesforce.js
+++ resources-from-salesforce/resources-from-salesforce.js
@@ -1,4 +1,4 @@
-// Load Resources From Objects - Salesforce v1.3
+// Load Resources From Objects - Salesforce v1.4
 
 // Purpose:
 // Loads resources in DayBack from Salesforce objects
@@ -52,21 +52,23 @@
 				objectName: 'User',
 				resourceFieldName: 'Name',
 				resourceFieldId: 'Id',
-				folderFieldName: 'Title',
-				tagsFieldName: 'Tags__c',
-				descriptionFieldName: 'Description__c',
-				sortFieldName: 'Sort__c',
-				selectedFieldName: 'Selected__c',
-				additionalCriteria: 'IsActive+=+true',
+				folderFieldName: 'Title', // optional
+				tagsFieldName: 'Tags__c', // optional
+				descriptionFieldName: 'Description__c', // optional
+				locationFieldName: 'Address', // optional
+				sortFieldName: 'Sort__c', // optional
+				selectedFieldName: 'Selected__c', // optional
+				colorFieldName: 'Color_Field__c', // optional
+				additionalCriteria: 'IsActive+=+true', // optional
 			},
 			{
 				objectName: 'Vehicle__c',
 				resourceFieldName: 'Name',
 				resourceFieldId: 'Id',
-				folderFieldName: 'Folder__c',
-				tagsFieldName: 'Tags__c',
-				descriptionFieldName: 'Description__c',
-				sortFieldName: 'Sort__c',
+				folderFieldName: 'Folder__c', // optional
+				tagsFieldName: 'Tags__c', // optional
+				descriptionFieldName: 'Description__c', // optional
+				sortFieldName: 'Sort__c', // optional
 			},
 		];
 
@@ -83,20 +85,26 @@
 
 	// Action code goes inside this function
 	function run() {
-		var folders;
-		var addedFolders = [];
-		var processedRequests = 0;
-		var resourceCount = 0;
-
 		//retrieve our canvas client object for authentication
-		var client = globals.fbk.client();
+		const client = globals.fbk.client();
 
 		//retrieve our canvas context object for REST links/urls
-		var context = globals.fbk.context();
+		const context = globals.fbk.context();
 
-		//create new array to overwrite resources
-		var resources = [];
+		const addedFolders = [];
+		let processedRequests = 0;
+		let resourceCount = 0;
+		let folders;
+
+		// Tell the calendar to use our resources
+		let resources = [];
 		globals.seedcodeCalendar.init('resources', resources);
+
+		//initialize a style element to hold our resource colors
+		let style = document.createElement('style');
+		style.type = 'text/css';
+		style.id = 'resource-colors';
+		document.head.appendChild(style);
 
 		//add none resource
 		resources.push(
@@ -118,11 +126,11 @@
 					folders,
 					resourceObject.folderFieldName
 				);
-				var folderIndex = {};
+				const folderIndex = {};
 
-				var records = data.payload.records;
-				var resource;
-				var category;
+				const records = data.payload.records;
+				let resource;
+				let category;
 
 				//create folders
 				folders.forEach(function (folder) {
@@ -137,7 +145,7 @@
 
 				//create resources
 				records.forEach(function (record) {
-					var folderName = getFieldValue(
+					const folderName = getFieldValue(
 						record,
 						resourceObject.folderFieldName
 					);
@@ -146,7 +154,7 @@
 					} else {
 						category = folderName;
 					}
-					var categories = category
+					const categories = category
 						? category.split(';')
 						: [category];
 					categories.forEach(function (thisCateogry) {
@@ -169,7 +177,12 @@
 								record,
 								resourceObject.descriptionFieldName
 							),
-							getFieldValue(record, resourceObject.sortFieldName)
+							getFieldValue(
+								record,
+								resourceObject.locationFieldName
+							),
+							getFieldValue(record, resourceObject.sortFieldName),
+							getFieldValue(record, resourceObject.colorFieldName)
 						);
 						resources.push(resource);
 						resourceCount++;
@@ -181,6 +194,12 @@
 				if (processedRequests === inputs.resourceObjects.length) {
 					//sort resources
 					resources = globals.dbk.filterFieldSort(resources);
+
+					//set the color of the resource
+					for (const resource of resources) {
+						assignColorToResource(resource, resource.color);
+					}
+
 					//continue actions/loading
 					confirmCallback();
 				}
@@ -189,38 +208,40 @@
 
 		function queryResources(resourceObject) {
 			//retrieve the query URL from context
-			var url = context.links.queryUrl;
+			const url = context.links.queryUrl;
 
-			var selectFields = [
+			const selectFields = [
 				resourceObject.resourceFieldName,
 				resourceObject.resourceFieldId,
 				resourceObject.folderFieldName,
 				resourceObject.tagsFieldName,
 				resourceObject.descriptionFieldName,
+				resourceObject.locationFieldName,
 				resourceObject.sortFieldName,
+				resourceObject.colorFieldName,
 				resourceObject.selectedFieldName,
 			]
 				.filter(Boolean)
 				.join(',');
 
 			//SOQL Select Statement Fields;
-			var select =
+			const select =
 				'SELECT+' + selectFields + '+FROM+' + resourceObject.objectName;
 
 			//SOQL Where Clause
 			//add additional criteria here as needed, e.g. Active=true, etc.
-			var where = resourceObject.additionalCriteria
+			const where = resourceObject.additionalCriteria
 				? '+WHERE+' + resourceObject.additionalCriteria
 				: '';
 
 			//SOQL query
-			var query = select + where;
+			const query = select + where;
 
 			//final URL for GET
-			var finalUrl = url + '?q=' + query;
+			const finalUrl = url + '?q=' + query;
 
 			//build settings object for Ajax call to Salesforce
-			var settings = {};
+			const settings = {};
 			settings.client = client;
 			settings.contentType = 'application/json';
 			settings.success = function (data) {
@@ -234,9 +255,9 @@
 		//helper functions
 
 		function buildFolders(data, folders, fieldName) {
-			var records = data.payload.records;
-			var hash = {};
-			var result = folders || [];
+			const records = data.payload.records;
+			const hash = {};
+			const result = folders || [];
 
 			result.forEach(function (folder) {
 				hash[folder] = true;
@@ -244,8 +265,8 @@
 
 			records.forEach(function (record) {
 				//handle related fields
-				var recordValue = getFieldValue(record, fieldName);
-				var categories;
+				const recordValue = getFieldValue(record, fieldName);
+				let categories;
 				if (recordValue) {
 					categories = recordValue.split(';');
 				} else {
@@ -254,7 +275,7 @@
 						: [];
 				}
 				categories.forEach(function (category) {
-					var thisResult = addFolder(hash, category);
+					const thisResult = addFolder(hash, category);
 					if (thisResult) {
 						result.push(thisResult);
 					}
@@ -280,24 +301,39 @@
 			selected,
 			tags,
 			description,
-			sort
+			location,
+			sort,
+			color
 		) {
-			var newResource = {};
+			const newResource = {};
+
 			newResource.name = name;
-			newResource.color = 'rgba(244, 244, 244, 0.85)';
+			newResource.color = color || stringToRGB(name);
 
 			if (folder) {
 				newResource.folderID = folder.folderID;
 				newResource.folderName = folder.folderName;
 			}
+
 			newResource.id = id ? id : globals.utilities.generateUID();
 			newResource.nameSafe = name;
 			newResource.shortName = name;
+
 			newResource.status = {
 				folderExpanded: false,
 				selected: selected || false,
 			};
+
 			newResource.description = description;
+
+			if (location) {
+				if (typeof location === 'object') {
+					newResource.location = `${location.street}, ${location.city}, ${location.postalCode}, ${location.country}`;
+				} else {
+					newResource.location = location;
+				}
+			}
+
 			newResource.sort = sort || (folder ? folder.sort + 1 : 0);
 
 			if (tags) {
@@ -308,12 +344,13 @@
 					}
 				});
 			}
+
 			return globals.dbk.mutateFilterField(newResource);
 		}
 
 		//creates folder and it's required attributes in the object
 		function createFolder(folderName) {
-			var folder = {};
+			const folder = {};
 			folder.folderID = globals.utilities.generateUID();
 			folder.name = folderName;
 			folder.folderName = folderName;
@@ -333,19 +370,20 @@
 			if (!fieldName) {
 				return '';
 			}
-			var splitField = fieldName.split('.');
-			var firstLevel = splitField[0];
+			const splitField = fieldName.split('.');
+			const firstLevel = splitField[0];
 			if (splitField.length === 1) {
 				//not a related field
 				return record[firstLevel] ? record[firstLevel] : '';
 			} else {
-				var levelCount = splitField.length - 1;
-				var string = JSON.stringify(record[firstLevel]);
-				var levelTest = (string.match(/attributes/g) || []).length;
-				var value = record;
+				const levelCount = splitField.length - 1;
+				const stringValue = JSON.stringify(record[firstLevel]);
+				const levelTest = (stringValue.match(/attributes/g) || [])
+					.length;
+				let value = record;
 				if (levelCount === levelTest) {
 					//drill for related field
-					for (var i = 0; i < splitField.length; i++) {
+					for (let i = 0; i < splitField.length; i++) {
 						value = value[splitField[i]];
 					}
 					return value;
@@ -353,6 +391,63 @@
 					return '';
 				}
 			}
+		}
+
+		//function to randomize resource color
+		function stringToRGB(inputString) {
+			let hash = 0;
+			if (inputString.length === 0) return hash;
+			for (const i in inputString) {
+				hash = inputString.charCodeAt(i) + ((hash << 5) - hash);
+				hash = hash & hash;
+			}
+			const rgb = [0, 0, 0];
+			for (let i = 0; i < 3; i++) {
+				const value = (hash >> (i * 8)) & 255;
+				rgb[i] = value;
+			}
+
+			// Make sure the color is not too light
+			// If it is, reduce the RGB values until it's dark enough
+			do {
+				// reduce the rgb values by 1 to make them darker
+				rgb[0] = Math.max(0, rgb[0] - 1);
+				rgb[1] = Math.max(0, rgb[1] - 1);
+				rgb[2] = Math.max(0, rgb[2] - 1);
+			} while (isColorTooLight(rgb[0], rgb[1], rgb[2]));
+
+			return `rgb(${rgb[0]}, ${rgb[1]}, ${rgb[2]})`;
+		}
+
+		/** @type {(r: number, g: number, b: number) => boolean} */
+		function isColorTooLight(r, g, b) {
+			// Calculate brightness using the luminance formula
+			const brightness = 0.299 * r + 0.587 * g + 0.114 * b;
+			return brightness > 200; // Adjust this threshold as needed
+		}
+
+		function assignColorToResource(resource, color) {
+			let resourceClasses = [];
+
+			//create a classname for the resource name
+			let resourceName = globals.utilities.stringToClass(
+				resource.nameSafe
+			);
+
+			//create a text color that goes best with event background color
+			//you may still need to adjust the background color slightly
+			let textColor = globals.utilities.generateTextColor(color);
+
+			resourceClasses.push(`
+              .fc-event.resource-${resourceName} {
+                  background-color:${color} !important; 
+                  color:${textColor} !important;
+                }`);
+
+			//create a style node and attach to main style sheet
+			let cssRules = resourceClasses.join('\n');
+
+			style.appendChild(document.createTextNode(cssRules));
 		}
 	}
 
