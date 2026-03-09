b9aff4edd1717f6fe7e9e8da0116da18de90559d
Tanner Ellen <tanner@seedcode.com>
2026-03-09 20:50:54 +0000

    refactor: removes unused variable assignment

--- maps/map-core/map-core-main-functions.js
+++ maps/map-core/map-core-main-functions.js
@@ -3,7 +3,7 @@
 // Purpose: Registers all of the functionality needed for calculating distances and routing
 // Action Type: On Startup
 // Prevent Default Action: No
-// Version: v1.1.6
+// Version: v1.1.7
 
 // More info on custom App Actions here:
 // https://docs.dayback.com/article/140-custom-app-actions
@@ -169,8 +169,6 @@
 
 		let routes = {};
 		let wasDestroyed = false;
-
-		let unscheduledFilterListener;
 
 		initialize();
 
